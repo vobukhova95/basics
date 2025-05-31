55801c26fada7df173782427f4fc63e937112b7e
Veronika Obukhova <vera.obukhova.95@gmail.com>
2025-05-31 23:45:02 +0700

    Create TestNG

--- src/test/java/ru/netology/service/CashbackHackServiceTest.java
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -1,10 +1,52 @@
 package ru.netology.service;
 
-import org.junit.jupiter.api.Test;
+import org.testng.annotations.Test;
 
-class CashbackHackServiceTest {
+import static org.testng.Assert.*;
+
+public class CashbackHackServiceTest {
 
     @Test
-    void remain() {
+    public void shouldReturn1000() {
+        CashbackHackService service = new CashbackHackService();
+        int amount = 0;
+
+        int actual = service.remain(amount);
+        int expected = 1000;
+
+        assertEquals(actual, expected);
+    }
+
+    @Test
+    public void shouldReturn1() {
+        CashbackHackService service = new CashbackHackService();
+        int amount = 999;
+
+        int actual = service.remain(amount);
+        int expected = 1;
+
+        assertEquals(actual, expected);
+    }
+
+    @Test
+    public void shouldReturn0() {
+        CashbackHackService service = new CashbackHackService();
+        int amount = 1000;
+
+        int actual = service.remain(amount);
+        int expected = 0;
+
+        assertEquals(actual, expected);
+    }
+
+    @Test
+    public void shouldReturn999() {
+        CashbackHackService service = new CashbackHackService();
+        int amount = 1001;
+
+        int actual = service.remain(amount);
+        int expected = 999;
+
+        assertEquals(actual, expected);
     }
 }
